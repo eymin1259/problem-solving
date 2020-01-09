9e704cf1e25027a103a5159d151aa03f764b7ce8
eymin1259 <eymin1259@gmail.com>
2020-01-09 13:29:44 +0900

    boj 입출력 11720

---
+++ boj-11720.cpp
@@ -0,0 +1,31 @@
+// 문제
+// N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+
+// 출력
+// 입력으로 주어진 숫자 N개의 합을 출력한다.
+
+// 예제 입력
+// 11
+// 10987654321
+// 예제 출력 
+// 46
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int len =0;
+    cin >> len;
+    string str;
+    cin >>str;
+    int sum =0;
+    for(int i=0; i<len; i++){
+        int val = (int)str[i]-48;
+        sum += val;
+    }
+    cout << sum <<endl;
+    return 0;
+}
