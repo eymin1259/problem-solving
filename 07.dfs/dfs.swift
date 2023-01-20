4d34f59050d19df8382f759bd5a222ff3f4f87c9
eymin1259 <54050609+eymin1259@users.noreply.github.com>
2023-01-20 19:49:00 +0900

    dfs 구현

---
+++ 07.dfs/dfs.swift
@@ -0,0 +1,42 @@
+/*
+dfs 구현
+*/
+
+import Foundation
+
+// 그래프는 인접행렬로 표현
+var graph : [[Int]] = [
+  [],
+  [2,3,8],
+  [1,7],
+  [1,4,5],
+  [3,5],
+  [3,4],
+  [7],
+  [2,6,8],
+  [1,7]
+]
+
+// 방문여부저장
+var dfsVisited = [Bool](repeating: false, count: graph.count)
+
+// dfs 함수
+func dfs(startNode:Int) {
+  dfsVisited[startNode] = true
+  print(startNode, terminator: " ")
+  var adjacents = graph[startNode]
+  for adjacent in adjacents {
+    if dfsVisited[adjacent] == false {
+      dfs(startNode:adjacent)
+    }
+  }
+}
+
+func dfsStart() {
+  print("dfs start")
+  dfs(startNode:1)
+  print("")
+  print("dfs end")
+}
+
+dfsStart()
