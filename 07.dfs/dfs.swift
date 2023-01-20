431ef263c5ccd5bd67248e268ade66d9cfdc63a8
eymin1259 <54050609+eymin1259@users.noreply.github.com>
2023-01-20 20:30:09 +0900

    Update dfs.swift

--- 07.dfs/dfs.swift
+++ 07.dfs/dfs.swift
@@ -1,5 +1,7 @@
 /*
-dfs 구현
+dfs sudo
+노드를 스택삽입, 방문처리 및 출력
+방문하지않은 인접노드들을 스택에 삽입
 */
 
 import Foundation
@@ -22,17 +24,21 @@ var dfsVisited = [Bool](repeating: false, count: graph.count)
 
 // dfs 함수
 func dfs(startNode: Int) {
+  // 노드 방문처리 및 출력 (함수호출됨 = 스택에 삽입된노드는 방문된 노드)
   dfsVisited[startNode] = true
   print(startNode, terminator: " ")
-  var adjacents = graph[startNode]
-  for adjacent in adjacents {
+
+  // 방문하지않은 인접노드들을 
+  for adjacent in  graph[startNode] {
     if dfsVisited[adjacent] == false {
+      // 스택에 삽입
       dfs(startNode: adjacent)
     }
   }
 }
 
 func dfsStart() {
+  print("")
   print("dfs start")
   dfs(startNode: 1)
   print("")
